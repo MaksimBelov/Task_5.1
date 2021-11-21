52d2d4e33b75e5c427c67bb973bd661adccc2200
Максим Белов <makbe@yandex.ru>
2021-11-21 09:47:57 +0300

    Добавьте файлы проекта.

---
+++ Task_5.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] array = new int[7];
+            float aMean = 0;
+            Console.WriteLine("Формируем одномерный массив из 7 целых чисел, введеных с клавиатуры:");
+            for (int i = 0; i < 7; i++)
+            {
+                Console.Write(" - введите число N{0} = ", i+1);
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine();
+            foreach (int i in array)
+            {
+                aMean += i;
+            }
+            aMean /= 7;
+            Console.WriteLine("Среднее арифметическое введенных числел равно {0:f2}", aMean);
+            Console.ReadKey();
+        }
+    }
+}
